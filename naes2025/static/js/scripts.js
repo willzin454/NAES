870a38f236bb474dba8baece490c485888b82f76
willzin454 <williamcarvalho493@gmail.com>
2025-04-03 20:54:31 -0300

    novas alterações

--- naes2025/static/js/scripts.js
+++ naes2025/static/js/scripts.js
@@ -1,10 +1,7 @@
 /*!
-* Start Bootstrap - Business Casual v7.0.9 (https://startbootstrap.com/theme/business-casual)
+* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
 * Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
 */
-// Highlights current date on contact page
-window.addEventListener('DOMContentLoaded', event => {
-    const listHoursArray = document.body.querySelectorAll('.list-hours li');
-    listHoursArray[new Date().getDay()].classList.add(('today'));
-})
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
--- naes2025/web/views.py
+++ naes2025/web/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 
+# View que apenas renderiza uma página Web
 from django.views.generic import TemplateView
 
 # Create your views here.
 
-# cria uma vie para redenrizar a pagina inicial
-# e faz uma herança de templateView
-
+# Cria uma view para renderizar a página inicial
+# e faz uma herança de TemplateView
 class PaginaInicial(TemplateView):
     template_name = "web/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["nome"] = "Rafael Zottesso"
+        return context
+
+
+class SobreView(TemplateView):
+    template_name = "paginasweb/sobre.html"
