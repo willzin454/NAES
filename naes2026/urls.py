a738453bac75cb2cc6ea2fd2d577249353efaf6c
willzin454 <williamcarvalho493@gmail.com>
2026-05-15 18:39:05 -0300

    Atualização das classes

--- naes2026/urls.py
+++ naes2026/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 from website import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('admin/', admin.site.urls),
-    path('sobre/', views.sobre, name='sobre'),
-    path('curso/', include('curso.urls')),
+    path("", views.index, name="index"),
+    path("admin/", admin.site.urls),
+    path("sobre/", views.sobre, name="sobre"),
+    path("curso/", include("curso.urls")),
 ]
--- naes2026/wsgi.py
+++ naes2026/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'naes2026.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "naes2026.settings")
 
 application = get_wsgi_application()
